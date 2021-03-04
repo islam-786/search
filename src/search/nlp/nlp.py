f598b101b38163c910d8a6c62eb81910a75b2a0a
axeemhaider <axeemhaider@gmail.com>
2021-03-04 08:33:31 +0500

    update

--- src/search/nlp/nlp.py
+++ src/search/nlp/nlp.py
@@ -1,2 +1,17 @@
+from .quran_qu import QuranQU
+from .hadith_qu import HadithQU
+
+
 class NLP:
-    pass
+    @classmethod
+    def analyze(cls, query):
+        quran_qu = QuranQU()
+        q = quran_qu.analyze(query)
+
+        hadith_qu = HadithQU()
+        h = hadith_qu.analyze(query)
+
+        if q["score"] >= h["score"]:
+            return q
+        else:
+            return h
