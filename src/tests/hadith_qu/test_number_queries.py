937af67508c86c2ca44ddcd7f1fa8a6a76057706
axeemhaider <axeemhaider@gmail.com>
2021-03-04 08:20:52 +0500

    number queries added for hadith test

---
+++ src/tests/hadith_qu/test_number_queries.py
@@ -0,0 +1,48 @@
+from search.hadith_qu import HadithQU
+
+# Queries
+#
+# user_query = "bukhari 123"
+# user_query = "bukhari number 123"
+# user_query = "bukhari #123"
+# user_query = "bukhari number #123"
+
+
+def test_query1():
+    user_query = "bukhari 123"
+    hadith_qu = HadithQU()
+    result = hadith_qu.analyze(user_query)
+
+    assert result["collection"] == "bukhari"
+    assert result["filters"][0]["name"] == "hadith_number"
+    assert result["filters"][0]["number"] == 123
+
+
+def test_query2():
+    user_query = "bukhari number 123"
+    hadith_qu = HadithQU()
+    result = hadith_qu.analyze(user_query)
+
+    assert result["collection"] == "bukhari"
+    assert result["filters"][0]["name"] == "hadith_number"
+    assert result["filters"][0]["number"] == 123
+
+
+def test_query3():
+    user_query = "bukhari #123"
+    hadith_qu = HadithQU()
+    result = hadith_qu.analyze(user_query)
+
+    assert result["collection"] == "bukhari"
+    assert result["filters"][0]["name"] == "hadith_number"
+    assert result["filters"][0]["number"] == 123
+
+
+def test_query4():
+    user_query = "bukhari number #123"
+    hadith_qu = HadithQU()
+    result = hadith_qu.analyze(user_query)
+
+    assert result["collection"] == "bukhari"
+    assert result["filters"][0]["name"] == "hadith_number"
+    assert result["filters"][0]["number"] == 123
